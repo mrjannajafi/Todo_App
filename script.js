b6ed15e278dcd509601f80d3ca8ee597463b56e0
mrjannajafi <njfymrjan@gmail.com>
2024-12-15 06:14:54 -0800

    updated

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const taskInp = document.querySelector(".task_input");
 const addBtn = document.querySelector(".add_btn");
-const ul = document.querySelector(".task_list");
+const tasklist = document.querySelector(".task_list");
 
 // function excuted when click add btn
-const add = function () {
+const addTaskItem = function () {
   // creat li
-  const li = document.createElement("li");
-  ul.appendChild(li);
-  li.textContent = taskInp.value;
-  li.classList.add("task");
+  const task = document.createElement("li");
+  tasklist.appendChild(task);
+  task.textContent = taskInp.value;
+  task.classList.add("task");
   // creat delete button
   const deleteBtn = document.createElement("i");
-  li.appendChild(deleteBtn);
+  task.appendChild(deleteBtn);
   deleteBtn.classList.add("delete_btn", "fa", "fa-trash");
   // creat check button
   const tikCheck = document.createElement("i");
-  li.appendChild(tikCheck);
+  task.appendChild(tikCheck);
   tikCheck.classList.add("check_btn", "fa", "fa-check-square");
   // check
   tikCheck.addEventListener("click", function () {
-    li.classList.toggle("compeleted");
+    task.classList.toggle("compeleted");
     tikCheck.classList.toggle("hidden");
   });
   // delete
   deleteBtn.addEventListener("click", function () {
-    li.classList.toggle("hidden");
+    tasklist.removeChild(task);
   });
 };
 
 addBtn.addEventListener("click", function () {
   if (taskInp.value !== "") {
-    add();
+    addTaskItem();
   } else {
     alert(` write your task please 😊 `);
   }
